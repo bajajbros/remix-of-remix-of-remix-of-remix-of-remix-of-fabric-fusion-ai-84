eccbe341742e2afe5cb2ec203737fc88c5b2e25e
bajajbros <70089871+bajajbros@users.noreply.github.com>
2026-01-28 04:53:25 +0530

    Updated Leads.tsx

--- src/pages/Leads.tsx
+++ src/pages/Leads.tsx
@@ -19,6 +19,7 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
